91d4de6c904501788f850f7fd2bf5bde73b79447
arcanjomitsuo <arcanjomitsuo@gmail.com>
2020-02-27 21:56:55 -0300

    Gestão de destinatários

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,67 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.string(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zip_code: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fail' });
+    }
+
+    const recipientExists = await Recipient.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (recipientExists) {
+      return res.status(400).json({ error: 'Recipient already exists' });
+    }
+
+    const recipient = await Recipient.create(req.body);
+    return res.json(recipient);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.number(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zipcode: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fail' });
+    }
+
+    const { email } = req.body;
+
+    const recipient = await Recipient.findByPk(req.params.id);
+
+    if (email && email !== Recipient.email) {
+      const userExists = await Recipient.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Recipients already exists.' });
+      }
+    }
+
+    const updatedRecipient = await recipient.update(req.body);
+
+    return res.json(updatedRecipient);
+  }
+}
+
+export default new RecipientController();
