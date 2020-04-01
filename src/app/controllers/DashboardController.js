f1758864d1ffe6f52a9a381d14323e9c49a9ab13
Rodrigo Arcanjo <rodrigo.arcanjo@afferolab.com.br>
2020-04-01 20:39:44 -0300

    Alterar status de encomendas

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -8,6 +8,8 @@ import {
   setSeconds,
   setMinutes,
   setHours,
+  startOfDay,
+  endOfDay,
 } from 'date-fns';
 
 import Deliveryman from '../models/Deliveryman';
@@ -131,18 +133,18 @@ class DashboardController {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
-    // Verifica se existe um entregador
+    // INICIO - Verifica se existe um entregador e uma encomenda
     const { deliveryman_id, delivery_id } = req.params;
 
     const deliverymanExists = await Deliveryman.findOne({
       where: { id: deliveryman_id },
     });
 
-    const deliveryExists = await Delivery.findOne({
+    const delivery = await Delivery.findOne({
       where: { id: delivery_id },
     });
 
-    if (!deliverymanExists && !deliveryExists) {
+    if (!deliverymanExists && !delivery) {
       return res
         .status(400)
         .json({ error: 'Delivery and Deliveryman does not exists' });
@@ -152,26 +154,57 @@ class DashboardController {
       return res.status(400).json({ error: 'Deliveryman does not exists' });
     }
 
-    if (!deliveryExists) {
+    if (!delivery) {
       return res.status(400).json({ error: 'Delivery does not exists' });
     }
+    // FIM - Verifica se existe um entregador e uma encomenda
 
-    const { start_date } = req.body;
+    const { start_date, end_date, signature_id } = req.body;
 
-    const initialDate = parseISO(start_date);
+    if (start_date) {
+      const initialDate = parseISO(start_date);
 
-    if (start_date && isBefore(initialDate, new Date()))
-      res.status(400).json({ error: 'Past dates are not allowed !' });
+      if (start_date && isBefore(initialDate, new Date()))
+        res.status(400).json({ error: 'Past dates are not allowed !' });
 
-    const initialHour = setSeconds(setMinutes(setHours(initialDate, 8), 0), 0);
-    const finalHour = setSeconds(setMinutes(setHours(initialDate, 18), 0), 0);
+      const initialHour = setSeconds(
+        setMinutes(setHours(initialDate, 8), 0),
+        0
+      );
+      const finalHour = setSeconds(setMinutes(setHours(initialDate, 18), 0), 0);
 
-    if (isAfter(initialDate, finalHour) || isBefore(initialDate, initialHour))
-      res
-        .status(400)
-        .json({ error: 'Delivery of orders only between 08:00 and 18:00' });
+      if (
+        isAfter(initialDate, finalHour) ||
+        isBefore(initialDate, initialHour)
+      ) {
+        res
+          .status(400)
+          .json({ error: 'Delivery of orders only between 08:00 and 18:00' });
+      }
 
-    return res.json({ ok: 'ok' });
+      const { count: numbersOfDeliveries } = await Delivery.findAndCountAll({
+        where: {
+          deliveryman_id,
+          start_date: {
+            [Op.between]: [startOfDay(initialDate), endOfDay(initialDate)],
+          },
+        },
+      });
+
+      if (numbersOfDeliveries >= 5)
+        res.status(400).json({
+          error: 'It is not possible to withdraw, daily limit reached.',
+        });
+
+      await delivery.update({ start_date });
+
+      return res.json({ message: 'Order withdrawn' });
+    }
+    if (end_date || signature_id) {
+      await delivery.update({ end_date, signature_id });
+      return res.json({ message: 'Delivery completed.' });
+    }
+    return res.json();
   }
 }
 
